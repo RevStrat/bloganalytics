ffa49e1f46b9454077476dd45c8acebd706f1d86
Rob Rankin <rob.rankin@revolutionstrategy.com>
2018-12-03 12:18:42 -0400

    Add object class tracking table

--- src/TrafficData.php
+++ src/TrafficData.php
@@ -6,7 +6,8 @@ use SilverStripe\ORM\DataObject;
 class TrafficData extends DataObject {
     private static $db = [
         'ObjectID' => 'Int',
-        'LastPeriodTraffic' => 'Int'
+        'LastPeriodTraffic' => 'Int',
+        'ObjectClass' => 'Varchar(255)'
     ];
     private static $table_name = 'TrafficData';
 }
